f8c5f125e819d10e19c46821751b5e99c6ab538f
michele <nikola.stanic.official@gmail.com>
2023-10-20 09:08:48 +0200

    Added all files and added new branches

--- src/main/java/TestFileOperations.java
+++ src/main/java/TestFileOperations.java
@@ -1,9 +1,18 @@
-
 /**
-* Created: 20.10.23
-*
-* 
-*/
+ * Created: 20.10.23
+ */
 
-package PACKAGE_NAME;public class TestFileOperations {
+public class TestFileOperations {
+    public static void main(String[] args) {
+        FileOperations fileOps = new FileOperations();
+        String filePath = "src/main/resources/output1.txt";
+
+        // Example usage:
+        String contentToWrite = "Hello, this is a test content to write to a file.";
+        fileOps.writeFile(contentToWrite);
+
+        String contentRead = fileOps.readFile(filePath);
+        System.out.println("Content read from the file:");
+        System.out.println(contentRead);
+    }
 }
